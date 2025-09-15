c2b33566dfd128e570aa600925db1690a27e5970
Natal Lima <nataldelima@gmail.com>
2025-09-15 19:16:32 -0400

    Criando a classe Posicao

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace xadrez;
+﻿using tabuleiro;
+
+namespace xadrez;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Posicao P;
+        P = new Posicao(3, 4);
+        Console.WriteLine($"Posição {P}");
     }
 }
