6ad657794d5600ae67027689d6feda6cd8a6e8de
Natal Lima <nataldelima@gmail.com>
2025-09-15 20:06:18 -0400

    Tratando erros com exceção personalizada

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -7,11 +7,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Tabuleiro tab = new Tabuleiro(8, 8);
-        tab.colocarPeca(new Torre(Cor.Preta, tab),new Posicao(0,0));
-        tab.colocarPeca(new Torre(Cor.Preta, tab),new Posicao(1,3));
-        tab.colocarPeca(new Rei(Cor.Preta, tab),new Posicao(2,4));
+        try
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            tab.colocarPeca(new Torre(Cor.Preta, tab),new Posicao(0,0));
+            tab.colocarPeca(new Rei(Cor.Preta, tab),new Posicao(0,2));
+            tab.colocarPeca(new Torre(Cor.Preta, tab),new Posicao(1,9));
+            tab.colocarPeca(new Rei(Cor.Preta, tab),new Posicao(2,4));
         
-        Tela.imprimirTabueiro(tab);
+            Tela.imprimirTabueiro(tab);
+
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+       
     }
 }
---
+++ xadrez/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,8 @@
+namespace tabuleiro;
+
+public class TabuleiroException : Exception
+{
+    public TabuleiroException(string msg) : base(msg)
+    {
+    }
+}
