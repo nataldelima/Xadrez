399ddb555c537933c6ea170eae2b570b3cb2a1ba
Natal Lima <nataldelima@gmail.com>
2025-09-18 18:20:19 -0400

    Implementando as restrições de movimento

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -12,25 +12,37 @@ class Program
             PartidaDeXadrez partida = new PartidaDeXadrez();
             while (!partida.terminada)
             {
-                Console.Clear();
-                Tela.imprimirTabueiro(partida.tab);
-                
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.Write("Origem:  ");
-                Posicao origem = Tela.lerPosicaoXadrex().toPosicao();
 
-                bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
-                
-                Console.Clear();
-                Tela.imprimirTabueiro(partida.tab, posicoesPossiveis);
-                
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.Write("Destino:  ");
-                Posicao destino = Tela.lerPosicaoXadrex().toPosicao();
-                
-                partida.ExecutaMovimento(origem, destino);
+                try
+                {
+                    Console.Clear();
+                    Tela.imprimirTabueiro(partida.tab);
+                    Console.WriteLine();
+                    Console.WriteLine($"Turno: {partida.turno}");
+                    Console.WriteLine($"Aguardando jogada: {partida.jogadorAtual}");
+
+                    Console.WriteLine();
+                    Console.Write("Origem:  ");
+                    Posicao origem = Tela.lerPosicaoXadrex().toPosicao();
+                    partida.validarPosicaoDeOrigem(origem);
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabueiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
+                    Console.Write("Destino:  ");
+                    Posicao destino = Tela.lerPosicaoXadrex().toPosicao();
+                    partida.validarPosicaoDeDestino(origem, destino);
+
+                    partida.realizaJogada(origem, destino);
+                }
+                catch (TabuleiroException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
             }
         }
         catch (TabuleiroException e)
