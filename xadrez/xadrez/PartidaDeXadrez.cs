84ddc93cf290921c771d4ee50675b2a939daccc5
Natal Lima <nataldelima@gmail.com>
2025-09-22 10:32:14 -0400

    Jogada especial promocao

--- xadrez/xadrez/PartidaDeXadrez.cs
+++ xadrez/xadrez/PartidaDeXadrez.cs
@@ -141,6 +141,22 @@ public class PartidaDeXadrez
             desfazMovimento(origem, destino, pecaCapturada);
             throw new TabuleiroException("Você não se pode colocar em xeque!");
         }
+        
+        Peca p = tab.peca(destino);
+        
+        // jogada especial promocao
+        if (p is Peao)
+        {
+            if (((p.cor == Cor.Branca && destino.linha == 0) || p.cor == Cor.Preta && destino.linha == 7))
+            {
+                p = tab.retirarPeca(destino);
+                pecas.Remove(p);
+                Peca dama = new Dama(p.cor, tab);
+                tab.colocarPeca(dama, destino);
+                pecas.Add(dama);
+            }
+        }
+        
 
         if (estaEmCheque(adversaria(jogadorAtual)))
         {
@@ -160,8 +176,7 @@ public class PartidaDeXadrez
             turno++;
             mudaJogador();
         }
-
-        Peca p = tab.peca(destino);
+        
         // jogada especial en passant
         if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
         {
